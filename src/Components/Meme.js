e0e556b05a4686251072603eab8ed2fea4231a6b
Kenneth Imade <kenneth.imade@yahoo.com>
2022-04-11 22:02:54 +0100

    added generate meme functionality

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,13 +1,27 @@
+import React from "react";
+import memesData from "../memesData"
+
 function Meme() {
+
+    const [memeImage, setMemeImage] = React.useState("");
+
+    function generateRandomMeme() {
+        const memesList = memesData.data.memes;
+        let randomMeme = memesList[ Math.floor(Math.random() * memesList.length)].url
+        setMemeImage((randomMeme))
+    }
+
     return (
-        <section>
-            <form className="meme--form">
+        <section className="meme--section">
+            <div className="meme--form">
                 <input type="text" placeholder="Top text" />
                 <input type="text" placeholder="Bottom text"/>
-                <button>
+                <button onClick={generateRandomMeme}>
                     Generate new meme 🖼️
                 </button>
-            </form>
+            </div>
+
+            <img alt="meme" src={memeImage} className="meme--img"/>
         </section>
     )
 }
